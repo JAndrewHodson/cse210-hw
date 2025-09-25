b61f1d9c49f3634fc12eeb2eea34aa3bee67d5b7
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-09-25 13:27:42 -0600

    looping code to ask user for integers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,15 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int lastAdded = 0;
+        Console.WriteLine("Enter a list of positive integers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            lastAdded = int.Parse(Console.ReadLine());
+        } while (lastAdded != 0);
     }
 }
