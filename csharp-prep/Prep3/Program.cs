b4566ed6832aab7ce939a7ca71db3d6d3c079bf7
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-09-25 12:29:21 -0600

    set up code to ask for magic number, one guess, and checks the guess once.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int response = int.Parse(Console.ReadLine());
+        if (response == number)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (response > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (response < number)
+        {
+            Console.WriteLine("Higher");
+        }
     }
 }
