1ee4e9bd1749c58bd621c496135a847830e2af7c
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-09-25 22:12:41 -0600

    added all functions, code asks for user name, year of birth, and favorite number. Calls functions to calculate user age, the square of their number, and returns that data to the user.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Globalization;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
+    //Function to welcome user to the program
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    //Funtion to prompt user for their name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    //Function to prompt user for user's favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    //Function to prompt user for their birth year
+    static int PromptUserBirthYear()
+    {
+        Console.Write("Please enter the year you were born: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    //Function to square user's favorite number
+    static int SquareNumber(int number)
+    {
+        number = number * number;
+        return number;
+    }
+
+    //functino to display results
+    static void DisplayResults(string name, int square, int age)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+        Console.WriteLine($"{name}, you have turned or will turn {age} this year.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int year = PromptUserBirthYear();
+        int age = 2025 - year;
+        int square = SquareNumber(userNumber);
+
+        DisplayResults(name, square, age);
+
     }
 }
