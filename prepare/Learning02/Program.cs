db1461d618cff87edb520272f4d47be23c9278fd
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-10-06 23:44:05 -0600

    made all changes and developments late, did not properly increment my pushes to git for version control

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Physics Professor";
+        job1._company = "Brigham Young University Idaho";
+        job1._startYear = 2017;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Editor";
+        job2._company = "Dragonsteel Publishers";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+
+        myResume._userName = "John Hodson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
     }
 }
