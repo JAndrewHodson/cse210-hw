c946d78c5273d0478289a5714a5f39445bd9e36d
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-11-19 04:18:30 -0600

    established classes, finalized entry

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,14 @@
+public class Entry
+{
+    private string entryPrompt = "";
+
+    private string DateTime = "";
+
+    private string userResponse = "";
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {DateTime} - Prompt: {entryPrompt}");
+        Console.WriteLine($"{userResponse}");
+    }
+}
