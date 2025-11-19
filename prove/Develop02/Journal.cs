8ab0b41ff2fb29c7473e21a3848479629fcb291e
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-11-19 04:23:22 -0600

    established journal, class, declared functions

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,19 @@
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void Display()
+    {
+
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void Load()
+    {
+        
+    }
+}
