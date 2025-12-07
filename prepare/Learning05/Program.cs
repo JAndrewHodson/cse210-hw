416da3d5536b727843d025abc1fbe6111bcd4400
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-12-07 16:00:25 -0700

    week 9 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Shape square = new Square("blue", 8);
+        Shape circle = new Circle("green", 4);
+        Shape rectangle = new Rectangle("red", 5, 4);
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape takes up {area} square units of space");
+        }
+
     }
 }
