376a77410a6b0339acd8b54fdb235396a11cd77c
JAndrewHodson <jandrewhodson2005@gmail.com>
2025-12-15 16:41:20 -0700

    created all classes, attributes, and behaviors as outlined in the final project design

---
+++ final/FinalProject/Feature.cs
@@ -0,0 +1,4 @@
+public class Feature
+{
+    private int _maxUses;
+}
---
+++ final/FinalProject/Spell.cs
@@ -0,0 +1,16 @@
+public class Spell
+{
+    private string _name;
+    private int _level;
+    private int _damage;
+    private string _damageType;
+
+    public void Display()
+    {
+        
+    }
+    public void Cast()
+    {
+        
+    }
+}
